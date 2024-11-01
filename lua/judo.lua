df9d74f0bfbed593f962e94f2358d1d10bdc065d
kris <kristoforusrp@gmail.com>
2024-11-01 07:57:09 +0700

    update special

--- lua/judo.lua
+++ lua/judo.lua
@@ -334,7 +334,7 @@ local function get_groups()
 		vCursor = { link = "Cursor" },
 		iCursor = { link = "Cursor" },
 		lCursor = { link = "Cursor" },
-		Special = { link = "JudoYellow" },
+		Special = { link = "JudoQuartz" },
 		Comment = { fg = colors.teal, italic = config.italic.comments },
 		Todo = { fg = colors.bg, bg = colors.yellow, bold = config.bold, italic = config.italic.comments },
 		Done = { fg = colors.orange, bold = config.bold, italic = config.italic.comments },
